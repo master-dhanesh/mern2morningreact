2ce8c0bb6b61fa2e3e42488d3098204c653fd348
master-dhanesh <dhanesh1296@gmail.com>
2022-06-16 08:37:46 +0530

    TWO WAY BINDING 02

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import { useState } from "react";
 
 const App = () => {
-    const [form, setForm] = useState({
-        username: "a",
-        password: "",
-    });
+    // const [form, setForm] = useState({
+    //     username: "a",
+    //     password: "",
+    // });
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    const ChangeHandler = (e) => {
-        const { value, name } = e.target;
-        setForm({ ...form, [name]: value });
+    // const ChangeHandler = (e) => {
+    //     const { value, name } = e.target;
+    //     setForm({ ...form, [name]: value });
+    // };
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        let newUser = { username, password };
+        console.log(newUser);
     };
+
     return (
         <div className="container mt-5 alert alert-dark">
-            <form>
+            <form onSubmit={SubmitHandler}>
                 <input
                     name="username"
                     placeholder="Username"
                     className="form-control w-25 mb-3"
                     type="text"
-                    onChange={ChangeHandler}
-                    value={form.username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
                 />
                 <input
                     name="password"
                     placeholder="Password"
                     className="form-control w-25 mb-3"
                     type="password"
-                    onChange={ChangeHandler}
-                    value={form.password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                 />
                 <button className="btn btn-primary w-25">Submit</button>
             </form>
