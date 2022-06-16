df91143acccbe86946f9dbae4e86a96d211b8f1c
master-dhanesh <dhanesh1296@gmail.com>
2022-06-16 08:32:31 +0530

    TWO WAY BINDING 01

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 import { useState } from "react";
-import Mydate from "./Components/Mydate";
-import Mytime from "./Components/Mytime";
 
 const App = () => {
-    const [mytime, setMytime] = useState(new Date().toLocaleTimeString());
-    const [mydate, setMydate] = useState(
-        new Date().toLocaleDateString("default", {
-            year: "numeric",
-            day: "numeric",
-            month: "short",
-            weekday: "short",
-        })
-    );
+    const [form, setForm] = useState({
+        username: "a",
+        password: "",
+    });
 
-    const [show, setShow] = useState(false);
-
-    const ClickHandler = () => {
-        setShow(!show);
+    const ChangeHandler = (e) => {
+        const { value, name } = e.target;
+        setForm({ ...form, [name]: value });
     };
-
     return (
-        <div className="container mt-5 alert alert-success">
-            <button
-                onClick={ClickHandler}
-                className={`btn btn-${show ? "success" : "primary"} w-25 `}
-            >
-                {show ? "Hide" : "Show"}
-            </button>
-            <hr className="w-50" />
-
-            {show ? (
-                <>
-                    <Mytime mytime={{ mytime, setMytime }} />
-                    <hr className="w-25" />
-                    <Mydate mydate={{ mydate, setMydate }} />
-                </>
-            ) : (
-                "Toggle button to see date time"
-            )}
+        <div className="container mt-5 alert alert-dark">
+            <form>
+                <input
+                    name="username"
+                    placeholder="Username"
+                    className="form-control w-25 mb-3"
+                    type="text"
+                    onChange={ChangeHandler}
+                    value={form.username}
+                />
+                <input
+                    name="password"
+                    placeholder="Password"
+                    className="form-control w-25 mb-3"
+                    type="password"
+                    onChange={ChangeHandler}
+                    value={form.password}
+                />
+                <button className="btn btn-primary w-25">Submit</button>
+            </form>
         </div>
     );
 };
