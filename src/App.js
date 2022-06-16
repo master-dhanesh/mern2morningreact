cf3a3fa6c25a37127d5090a6af150c8e8f844829
master-dhanesh <dhanesh1296@gmail.com>
2022-06-16 08:41:48 +0530

    GETTING FORM DATA WITHOUT BINDING

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { useState } from "react";
-
 const App = () => {
-    // const [form, setForm] = useState({
-    //     username: "a",
-    //     password: "",
-    // });
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    // const ChangeHandler = (e) => {
-    //     const { value, name } = e.target;
-    //     setForm({ ...form, [name]: value });
-    // };
-
     const SubmitHandler = (e) => {
         e.preventDefault();
-        let newUser = { username, password };
+        let [username, password] = e.target;
+        let newUser = { username: username.value, password: password.value };
         console.log(newUser);
     };
 
@@ -27,16 +14,12 @@ const App = () => {
                     placeholder="Username"
                     className="form-control w-25 mb-3"
                     type="text"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
                 />
                 <input
                     name="password"
                     placeholder="Password"
                     className="form-control w-25 mb-3"
                     type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
                 />
                 <button className="btn btn-primary w-25">Submit</button>
             </form>
