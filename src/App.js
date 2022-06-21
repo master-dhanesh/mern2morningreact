3e0be59a9e5215a53c42d78a12b48398eaf5d75f
master-dhanesh <dhanesh1296@gmail.com>
2022-06-21 08:53:56 +0530

    CLASS COMPONENT LIFECYCLES

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import { useState } from "react";
-import Create from "./Components/Create";
+import axios from "axios";
+import React, { Component } from "react";
 import Show from "./Components/Show";
 
-const App = () => {
-    const [todos, setTodos] = useState(
-        JSON.parse(localStorage.getItem("todos")) || []
-    );
+class App extends Component {
+    state = {
+        show: true,
+        posts: [],
+    };
 
-    return (
-        <div className="container mt-5">
-            <Create
-                todos={todos}
-                setTodos={setTodos}
-                about="This is for checking purpose"
-            />
-            <hr className="w-50 m-auto mt-3 mb-3" />
-            <Show todos={todos} setTodos={setTodos} />
-        </div>
-    );
-};
+    constructor() {
+        super();
+        console.log("--CONSTRUCTOR APP.JS");
+    }
+
+    render() {
+        console.log("--RENDER APP.JS", this.state);
+        return (
+            <div className="container mt-5 alert alert-success p-5">
+                <button
+                    onClick={() => this.setState({ show: !this.state.show })}
+                    className="btn btn-primary"
+                >
+                    {this.state.show ? "Hide" : "Show"}
+                </button>
+                {this.state.show && <Show />}
+                <hr />
+                {this.state.posts.map((post) => (
+                    <p key={post.id}>{post.title}</p>
+                ))}
+            </div>
+        );
+    }
+
+    async componentDidMount() {
+        console.log("--COMPONENTDIDMOUNT APP.JS");
+        this.fetchPosts();
+    }
+
+    fetchPosts = async () => {
+        const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts`
+        );
+        this.setState({ posts: data });
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("COMPONENTDIDUPDATE APP.JS", prevProps, prevState);
+    }
+}
 
 export default App;
+
+// import axios from "axios";
+
+// const App = () => {
+//     const fetchPosts = async () => {
+//         const { data } = await axios.get(
+//             `https://jsonplaceholder.typicode.com/posts`
+//         );
+//         console.log(data);
+//     };
+
+//     return (
+//         <div className="container mt-5">
+//             <button onClick={fetchPosts} className="btn btn-primary">
+//                 Fetch Posts
+//             </button>
+//         </div>
+//     );
+// };
+
+// export default App;
--- src/Components/Show.js
+++ src/Components/Show.js
@@ -1,43 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Show = (props) => {
-    const { todos, setTodos } = props;
-
-    const DeleteHandler = (id) => {
-        const filterdTodos = todos.filter((todo) => todo._id !== id);
-        setTodos(filterdTodos);
-        localStorage.setItem("todos", JSON.stringify(filterdTodos));
-    };
-
-    let todoslist = (
-        <p className="text-center text-danger">No todos present.</p>
-    );
-
-    if (todos.length > 0) {
-        todoslist = todos.map((todo) => (
-            <li
-                key={todo._id}
-                className="list-group-item d-flex justify-content-between align-items-start"
-            >
-                <div className="ms-2 me-auto">
-                    <div className="fw-bold">{todo.item}</div>
-                </div>
-                <span
-                    onClick={() => DeleteHandler(todo._id)}
-                    role="button"
-                    className="badge rounded-pill"
-                >
-                    ❌
-                </span>
-            </li>
-        ));
+export default class Show extends Component {
+    componentWillUnmount() {
+        console.log("COMPONENTWILLUNMOUNT SHOW.JS");
+        alert("Do you want to leave this page?");
     }
 
-    return (
-        <ol className="w-50 m-auto list-group list-group-numbered">
-            {todoslist}
-        </ol>
-    );
-};
-
-export default Show;
+    render() {
+        return (
+            <div className="alert alert-danger mt-3">
+                This is Show Component
+            </div>
+        );
+    }
+}
